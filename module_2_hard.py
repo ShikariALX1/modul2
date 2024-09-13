06abe1fffd6a8e601dd5175b220274d83544956a
Александр <aleksanderal.92@mail.ru>
2024-09-13 12:34:29 +0300

    Изменен на подбор пароля.

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,21 @@
 import random
 
-pass_ = ({3: 12, 4: 13, 5: 1423, 6: 121524, 7: 162534, 8: 13172635, 9: 1218273645, 10: 141923283746,
-          11: 11029384756, 12: 12131511124210394857, 13: 112211310495867, 14: 1611325212343114105968,
-          15: 1214114232133124115106978, 16: 1317115262143531341251161079, 17: 11621531441351261171089,
-          18: 12151811724272163631545414513612711810, 19: 118217316415514613712811910,
-          20: 13141911923282183731746416515614713812911})
+
+def pass_(n):
+    result = ''
+    for i in range(1, n):
+        for j in range(1, n):
+            if (((i + j) == n or n % (i + j) == 0)
+                    and (f'{i}+{j}' not in result and f'{j}+{i}' not in result)
+                    and i != j):
+                result += str(i) + '+' + str(j) + ' '
+    result = result.replace('+', '')
+    result = result.replace(' ', '')
+    return result
+
 
 while True:
-    result = random.choice(list(pass_))
-    print(f'{result} - {pass_.get(result)}')
+    pole_1 = random.choice(range(3, 21))
+    pole_2 = pass_(int(pole_1))
+    print(f'Пароль: {pole_1} {pole_2}')
     break
