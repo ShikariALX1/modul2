c465b6e5f6808c90f93b3a02c71ee225f1339ca2
Александр <aleksanderal.92@mail.ru>
2024-09-16 08:57:37 +0300

    Исправлен второй цикл.

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,10 +4,8 @@ import random
 def pass_(n):
     result = ''
     for i in range(1, n):
-        for j in range(1, n):
-            if (((i + j) == n or n % (i + j) == 0)
-                    and (f'{i}+{j}' not in result and f'{j}+{i}' not in result)
-                    and i != j):
+        for j in range(i + 1, n):
+            if ((i + j) == n or n % (i + j) == 0):
                 result += str(i) + '+' + str(j) + ' '
     result = result.replace('+', '')
     result = result.replace(' ', '')
